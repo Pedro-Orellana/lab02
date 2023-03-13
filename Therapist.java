55c9675f101b873bd226b013af3821b62ff230a0
Pedro Orellana <pedrorellana@Pedros-MacBook-Pro.local>
2023-03-13 11:48:33 -0700

    add new todo line

--- Therapist.java
+++ Therapist.java
@@ -4,11 +4,21 @@ public class Therapist {
 
     public static Scanner scanner = new Scanner(System.in);
 
-    public static String[] questions = { 
-        "What's making you feel replace today? (you don't know, personal issue, personal achievement, something else?)",
-        "Tell me more about it, what is this \"replace\" you are talking about (options)",
-        "That's great! I am happy for you! is there anything else you want to talk about? (yes, no)"
-     };
+    public static String[] questions = {
+            "What's making you feel replace today? (you don't know, personal issue, personal achievement, something else?)",
+            "Tell me more about it, what is this \"replace\" you are talking about (problem at work, family problem, a promotion at work, succeeding in school)",
+            "That's great! I am happy for you! is there anything else you want to talk about? (yes, no)"
+    };
+
+    public static String[] answers1 = {
+        "I don't know",
+        "personal issue",
+        "personal achievement"
+    };
+
+    public static String[] answers2 = {
+        "problem at work"
+    };
 
     public static void main(String[] args) {
 
@@ -20,10 +30,9 @@ public class Therapist {
             response = scanner.nextLine();
             wantsToContinue = therapySession(response);
         }
-        
-        
+
     }
- 
+
     public static String normalizeResponse(String response) {
 
         // TODO: finish this method
@@ -37,8 +46,6 @@ public class Therapist {
         return normalizeResponse(response);
     }
 
-    
-    
     public static Boolean therapySession(String response) {
 
         int i = 0;
@@ -47,23 +54,21 @@ public class Therapist {
             System.out.print(currentQuestion + " ");
             response = scanner.nextLine();
             i++;
-            if(i == questions.length) {
+            if (i == questions.length) {
                 break;
             }
         }
-        //TODO: check if the user really wants to continue or if they input an invalid option
-        
-        if(response.equals("no") || response.equals("goodbye")) {
+        // TODO: check if the user really wants to continue or if they input an invalid
+        //TODO: This is another change that I just made
+        // option
+
+        if (response.equals("no") || response.equals("goodbye")) {
             System.out.println("Okay then, this is the end of our session, have a great day!");
             return false;
-        } 
+        }
 
         return true;
-      
+
     }
-
-
-
-
 
 }
