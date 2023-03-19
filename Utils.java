63adeb9e0c24f2411d2e46d1f653bfccf4fddd4c
Pedro <pedrorellana21@gmail.com>
2023-03-19 01:15:00 -0700

    implement unfinished methods

--- Utils.java
+++ Utils.java
@@ -2,10 +2,12 @@ public class Utils {
 
     private String[] questions = {
             "What's making you feel replace today? (you don't know, personal issue, personal achievement, something else?)",
-            "Tell me more about it, what is this \"replace\" you are talking about (problem at work, family problem, a promotion at work, success in school)",
+            "Tell me more about it, what is this \"replace\" you are talking about (problem at work, family problem, promotion at work, success in school)",
             "Can you tell me more about this replace?",
-            "That's great! I am happy for you! is there anything else you want to talk about? (yes, no)",
-            "Oh no, I'm sorry to hear that, but things will eventually get better again!, is there anything else you want to talk about? (yes, no)"
+            "That's great! I am happy for you! and besides replace, how else is this making you feel? (joyful, hopeful for the future, grateful to life, motivated)",
+            "It's really good to feel replace, keep doing what you are doing and you will continue to get success in your life! is there anything else you would like to talk about today? (yes, no)",
+            "Oh no, I'm sorry to hear that, I know it must be a hard situation, but besides replace, how else are you feeling about this situation? (anxious, unmotivated, tired of this situation, burned out)",
+            "I understand, feeling replace it's not a nice feeling, but know that life has ups and downs, and this will also pass, stay strong and rely on your family and friends! is there anything else you would like to talk about? (yes, no)"
     };
 
     public String[] getQuestions() {
@@ -25,7 +27,7 @@ public class Utils {
         return true;
     }
 
-    private String[] answers1 = {
+    private String[] answers0 = {
             "I don't know",
             "I do not know",
             "dunno",
@@ -34,21 +36,45 @@ public class Utils {
             "something else"
     };
 
-    public Boolean isAnswer1Valid(String answer) {
+    public boolean isAnswer0Valid(String answer) {
         // TODO: loop through the answers1 array, and if the answer is none of the
         // answers in the array, return false, otherwise return true
         return true;
     }
 
-    private String[] answers2 = {
+    private String[] answers1 = {
             "problem at work",
             "family problem",
             "promotion at work",
             "success in school"
     };
 
-    public Boolean isAnswer2Valid(String answer) {
-        // same as previous method but for anwers2 array
+    public boolean isAnswer1Valid(String answer) {
+        // TODO: same as previous method but for anwers2 array
+        return true;
+    }
+
+    private String[] answers3 = {
+            "joyful",
+            "hopeful for the future",
+            "grateful to life",
+            "motivated"
+    };
+
+    public boolean isAnswer3Valid(String answer) {
+        // TODO: please finish this method
+        return true;
+    }
+
+    private String[] answer5 = {
+            "anxious",
+            "unmotivated",
+            "tired of this situation",
+            "burned out"
+    };
+
+    public boolean isAnswer5Valid(String answer) {
+        // TODO: please finish this method
         return true;
     }
 
@@ -79,7 +105,7 @@ public class Utils {
         switch (answer) {
             case "problem at work":
             case "family problem":
-                return 1;
+                return 2;
             case "promotion at work":
             case "success in school":
                 return 0;
